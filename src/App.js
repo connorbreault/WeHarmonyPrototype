bea0fe832679ff26015d39cc1ccfb34538663123
rhernandez <rhernandez@skyslope.com>
2019-11-12 06:35:39 -0800

    reverted my changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import React from "react";
-import Container from "./components/Container";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Login from "./pages/Login/Login.js"
+import Home from "./pages/Home/Home.js";
+import Signup from "./pages/Signup/Signup.js";
+import Profile from "./pages/Profile/Profile.js";
+import Contact from "./pages/Contact/Contact.js"
 
-function App() {
-  return <Container />;
-}
+// import Container from "./components/Container";
+
+// function App() {
+//   return <Container />;
+// }
+
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <Route exact path="/" component={Login} />
+        <Route exact path="/Home" component={Home} />
+        <Route exact path="/Signup" component={Signup} />
+        <Route exact path="/Profile" component={Profile} />
+        <Route exact path="/Contact" component={Contact} />
+      </div>
+    </Router>
+  );
+};
 
 export default App;
--- src/base.js
+++ src/base.js
@@ -1,4 +1,6 @@
-import firebase from 'firebase';
+// import firebase from 'firebase';
+import firebase from "react-native-firebase";
+import "firebase/auth";
 
 const config = {
     apiKey: process.env.REACT_APP_FIREBASE_KEY,
