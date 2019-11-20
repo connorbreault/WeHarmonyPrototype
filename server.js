fddda1f50deb8557474c795cd3d42360a0422462
rhernandez <rhernandez@skyslope.com>
2019-11-19 18:45:35 -0800

    heroku

--- server.js
+++ server.js
@@ -1,58 +1,63 @@
-// import express from "express";
-// import mongoose from "mongoose";
-// import mongojs from "mongojs";
-// import exphbs from "express-handlebars";
-// import axios from "axios";
-// import cheerio from "cheerio";
-// import logger from "morgan";
+import express from "express";
+import mongoose from "mongoose";
+import exphbs from "express-handlebars";
+import axios from "axios";
+import cheerio from "cheerio";
+import logger from "morgan";
 
-// var app = express();
-// var PORT = process.env.PORT || 3001;
+const Data = require('./data');
 
-// // Use morgan logger for logging requests
-// app.use(logger("dev"));
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-// app.use(express.static("public"));
+var app = express();
+var PORT = process.env.PORT || 3001;
 
-// // Connect to the Mongo DB
-// var MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://rebeccahdz10:<password>@weharmony-rg9xb.mongodb.net/test?retryWrites=true&w=majority'
-// mongoose.connect(MONGODB_URI)
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
 
-// var databaseURL = "mongodb+srv://rebeccahdz10:<password>@weharmony-rg9xb.mongodb.net/test?retryWrites=true&w=majority";
+// Connect to the Mongo DB
+var MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://weharmony:<password>@weharmony-rg9xb.mongodb.net/test?retryWrites=true&w=majority'
+mongoose.connect(MONGODB_URI)
+
+var db = mongoose.connection;
+
+// var databaseURL = "mongodb+srv://weharmony:<password>@weharmony-rg9xb.mongodb.net/test?retryWrites=true&w=majority";
 
 // var db = mongojs(databaseURL, articles);
 
-// db.on("error", function (error) {
-//     console.log("There was an error:", error);
-// });
+db.once('open', () => console.log('connected to the database'));
 
-// app.get("/", function (req, res) {
-//     console.log("You've reached the mongo db.");
-// });
+db.on("error", () => console.log("There was an error:", error));
 
-// app.get("/user/:id", function (req, res) {
-//     db.getUser.find({}, function (err, found) {
-//         if (err) {
-//             console.log(err);
-//         }
-//         else {
-//             console.log(found);
-//         }
-//     });
-// });
+app.get("/", function (req, res) {
+    Data.find((err, data) => {
+        if (err) return res.json({ success: false, error: err });
+        return res.json({ success: true, data: data });
+    });
 
-// // app.get("/reset", function (req, res) {
-// //     db.scrapeAltPress.drop()
-// //     res.send("reset");
-// // });
+    app.get("/user/:id", function (req, res) {
+        db.getUser.find({}, function (err, found) {
+            if (err) {
+                console.log(err);
+            }
+            else {
+                console.log(found);
+            }
+        });
+    });
 
-// // app.get("/", function (req, res) {
-// //     axios.get("/").then(function (response) {
-// //     });
-// // });
+    // app.get("/reset", function (req, res) {
+    //     db.scrapeAltPress.drop()
+    //     res.send("reset");
+    // });
 
-// // Start the server
-// app.listen(PORT, function () {
-//     console.log("App running on port " + PORT + "!");
-// });
+    // app.get("/", function (req, res) {
+    //     axios.get("/").then(function (response) {
+    //     });
+});
+
+// Start the server
+app.listen(PORT, function () {
+    console.log("App running on port " + PORT + "!");
+});
